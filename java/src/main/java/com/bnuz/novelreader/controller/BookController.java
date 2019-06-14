283dd8757084ed1d42cf63bb84e96b70dd6b5210
HarryBlackCatQAQ <641779160@qq.com>
2019-06-14 16:10:14 +0800

    在接口上添加了出过来的内容输出

--- java/src/main/java/com/bnuz/novelreader/controller/BookController.java
+++ java/src/main/java/com/bnuz/novelreader/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -16,7 +17,7 @@ public class BookController {
 
     @PostMapping(value = "/searching")
     public List<Book> getBookList(String content){
-
+        System.out.println("你传给我了:" + content);
         return null;
     }
 }
